d3f53cc2edbb70a1685eeec82a33a2ebab68b6bb
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-22 10:19:28 -0500

    Paginate Products query

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,55 @@
 import Product from '../models/product.js';
 
 export const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({});
+	const search = 'a';
+	const products = await Product.find({})
+		.sort('name')
+		.select('name price')
+		.limit(10)
+		.skip(5);
 	res.status(200).json({ products, nbHits: products.length });
 };
 
 export const getAllProducts = async (req, res) => {
-	const { featured } = req.query;
-	let queryObject = {};
+	const { featured, company, name, sort, fields } = req.query;
+	const queryObject = {};
 
 	if (featured) {
 		queryObject.featured = featured === 'true';
 	}
 
-	console.log(queryObject);
+	if (company) {
+		queryObject.company = company;
+	}
 
-	const products = await Product.find(queryObject);
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i' };
+	}
+
+	//console.log(queryObject);
+
+	let result = Product.find(queryObject);
+	// sort
+	if (sort) {
+		const sortList = sort.split(',').join(' ');
+		result = result.sort(sortList);
+	} else {
+		result = result.sort('createdAt');
+	}
+
+	// fields
+	if (fields) {
+		const fieldsList = fields.split(',').join(' ');
+		result = result.select(fieldsList);
+	}
+
+	// pagination
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+	result = result.skip(skip).limit(limit);
+
+	const products = await result;
+
 	res.status(200).json({ products, nbHits: products.length });
 };
